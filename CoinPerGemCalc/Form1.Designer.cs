ceaf14415796a50d3e35675f5a4e81cfb11f12a9
NotJansel <janselfritz096@gmail.com>
2021-11-18 18:58:26 +0100

    Projektdateien hinzufügen.

---
+++ CoinPerGemCalc/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace CoinPerGemCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.package_1 = new System.Windows.Forms.Button();
+            this.package_2 = new System.Windows.Forms.Button();
+            this.package_3 = new System.Windows.Forms.Button();
+            this.package_4 = new System.Windows.Forms.Button();
+            this.package_5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // package_1
+            // 
+            this.package_1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.package_1.Location = new System.Drawing.Point(13, 13);
+            this.package_1.Name = "package_1";
+            this.package_1.Size = new System.Drawing.Size(102, 37);
+            this.package_1.TabIndex = 0;
+            this.package_1.Text = "Package $5,94";
+            this.package_1.UseVisualStyleBackColor = true;
+            this.package_1.Click += new System.EventHandler(this.package_1_Click);
+            // 
+            // package_2
+            // 
+            this.package_2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.package_2.Location = new System.Drawing.Point(121, 13);
+            this.package_2.Name = "package_2";
+            this.package_2.Size = new System.Drawing.Size(102, 37);
+            this.package_2.TabIndex = 1;
+            this.package_2.Text = "Package $11,89";
+            this.package_2.UseVisualStyleBackColor = true;
+            this.package_2.Click += new System.EventHandler(this.package_2_Click);
+            // 
+            // package_3
+            // 
+            this.package_3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.package_3.Location = new System.Drawing.Point(229, 12);
+            this.package_3.Name = "package_3";
+            this.package_3.Size = new System.Drawing.Size(102, 37);
+            this.package_3.TabIndex = 2;
+            this.package_3.Text = "Package $29,74";
+            this.package_3.UseVisualStyleBackColor = true;
+            this.package_3.Click += new System.EventHandler(this.package_3_Click);
+            // 
+            // package_4
+            // 
+            this.package_4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.package_4.Location = new System.Drawing.Point(337, 13);
+            this.package_4.Name = "package_4";
+            this.package_4.Size = new System.Drawing.Size(102, 37);
+            this.package_4.TabIndex = 3;
+            this.package_4.Text = "Package $59,49";
+            this.package_4.UseVisualStyleBackColor = true;
+            this.package_4.Click += new System.EventHandler(this.package_4_Click);
+            // 
+            // package_5
+            // 
+            this.package_5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.package_5.Location = new System.Drawing.Point(445, 13);
+            this.package_5.Name = "package_5";
+            this.package_5.Size = new System.Drawing.Size(102, 37);
+            this.package_5.TabIndex = 4;
+            this.package_5.Text = "Package $118,99";
+            this.package_5.UseVisualStyleBackColor = true;
+            this.package_5.Click += new System.EventHandler(this.package_5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Booster Cookie Sell Price: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Coins per Gem: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(324, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Coins worth of gems in this Package: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Last API Update: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(503, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Coins recieved by selling cookies recieved in this Package: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.package_5);
+            this.Controls.Add(this.package_4);
+            this.Controls.Add(this.package_3);
+            this.Controls.Add(this.package_2);
+            this.Controls.Add(this.package_1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Coins per Gem-Package";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button package_1;
+        private System.Windows.Forms.Button package_2;
+        private System.Windows.Forms.Button package_3;
+        private System.Windows.Forms.Button package_4;
+        private System.Windows.Forms.Button package_5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CoinPerGemCalc/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace CoinPerGemCalc
+{
+    public partial class Form1 : Form
+    {
+        public static double cookieinstasell, coinspergem, totalcoins, usdpergem, dollarworthofgems, coinsperdollar;
+        public static double priceinstore, geminpackage, recievedcoins, cookiesinpackage, coinswithcookies;
+        public static long unixtime;
+
+        private void package_1_Click(object sender, EventArgs e)
+        {
+            priceinstore = 5.94;
+            geminpackage = 675;
+            coinspergem = Math.Round(cookieinstasell / 325, 2);
+            usdpergem = priceinstore / geminpackage;
+            dollarworthofgems = 1 / usdpergem;
+            coinsperdollar = dollarworthofgems * coinspergem;
+            recievedcoins = Math.Round(coinsperdollar * priceinstore, 2);
+            cookiesinpackage = geminpackage / 325;
+            coinswithcookies = (int) cookiesinpackage * cookieinstasell;
+            label5.Text = "Coins recieved by selling cookies recieved in this Package: " + (Math.Round(coinswithcookies, 2)).ToString();
+            label2.Text = "Coins per Gem: " + coinspergem.ToString();
+            label3.Text = "Coins worth of gems in this Package: " + recievedcoins.ToString();
+        }
+
+        private void package_2_Click(object sender, EventArgs e)
+        {
+            priceinstore = 11.89;
+            geminpackage = 1390;
+            coinspergem = Math.Round(cookieinstasell / 325, 2);
+            usdpergem = priceinstore / geminpackage;
+            dollarworthofgems = 1 / usdpergem;
+            coinsperdollar = dollarworthofgems * coinspergem;
+            recievedcoins = Math.Round(coinsperdollar * priceinstore, 2);
+            cookiesinpackage = geminpackage / 325;
+            coinswithcookies = (int) cookiesinpackage * cookieinstasell;
+            label5.Text = "Coins recieved by selling cookies recieved in this Package: " + (Math.Round(coinswithcookies, 2)).ToString();
+            label2.Text = "Coins per Gem: " + coinspergem.ToString();
+            label3.Text = "Coins worth of gems in this Package: " + recievedcoins.ToString();
+        }
+
+        private void package_3_Click(object sender, EventArgs e)
+        {
+            priceinstore = 29.74;
+            geminpackage = 3600;
+            coinspergem = Math.Round(cookieinstasell / 325, 2);
+            usdpergem = priceinstore / geminpackage;
+            dollarworthofgems = 1 / usdpergem;
+            coinsperdollar = dollarworthofgems * coinspergem;
+            recievedcoins = Math.Round(coinsperdollar * priceinstore, 2);
+            cookiesinpackage = geminpackage / 325;
+            coinswithcookies = (int) cookiesinpackage * cookieinstasell;
+            label5.Text = "Coins recieved by selling cookies recieved in this Package: " + (Math.Round(coinswithcookies, 2)).ToString();
+            label2.Text = "Coins per Gem: " + coinspergem.ToString();
+            label3.Text = "Coins worth of gems in this Package: " + recievedcoins.ToString();
+        }
+
+        private void package_4_Click(object sender, EventArgs e)
+        {
+            priceinstore = 59.49;
+            geminpackage = 7300;
+            coinspergem = Math.Round(cookieinstasell / 325, 2);
+            usdpergem = priceinstore / geminpackage;
+            dollarworthofgems = 1 / usdpergem;
+            coinsperdollar = dollarworthofgems * coinspergem;
+            recievedcoins = Math.Round(coinsperdollar * priceinstore, 2);
+            cookiesinpackage = geminpackage / 325;
+            coinswithcookies = (int) cookiesinpackage * cookieinstasell;
+            label5.Text = "Coins recieved by selling cookies recieved in this Package: " + (Math.Round(coinswithcookies, 2)).ToString();
+            label2.Text = "Coins per Gem: " + coinspergem.ToString();
+            label3.Text = "Coins worth of gems in this Package: " + recievedcoins.ToString();
+        }
+
+        private void package_5_Click(object sender, EventArgs e)
+        {
+            priceinstore = 118.99;
+            geminpackage = 16400;
+            coinspergem = Math.Round(cookieinstasell / 325, 2);
+            usdpergem = priceinstore / geminpackage;
+            dollarworthofgems = 1 / usdpergem;
+            coinsperdollar = dollarworthofgems * coinspergem;
+            recievedcoins = Math.Round(coinsperdollar * priceinstore, 2);
+            cookiesinpackage = geminpackage / 325;
+            coinswithcookies = (int) cookiesinpackage * cookieinstasell;
+            label5.Text = "Coins recieved by selling cookies recieved in this Package: " + (Math.Round(coinswithcookies, 2)).ToString();
+            label2.Text = "Coins per Gem: " + coinspergem.ToString();
+            label3.Text = "Coins worth of gems in this Package: " + recievedcoins.ToString();
+        }
+
+        public static string apiresponse, cookieconv, unixconv;
+        public static dynamic cookiesellraw, unixdyn;
+        public static WebClient client = new WebClient();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            apiresponse = client.DownloadString("https://api.hypixel.net/skyblock/bazaar");
+            var apijson = JsonConvert.DeserializeObject<dynamic>(apiresponse);
+            cookiesellraw = apijson.products.BOOSTER_COOKIE.quick_status.sellPrice;
+            unixdyn = apijson.lastUpdated;
+            unixconv = unixdyn.ToString();
+            unixtime = long.Parse(unixconv);
+            label4.Text = "Last API Update: " + DateTimeOffset.FromUnixTimeMilliseconds(unixtime).LocalDateTime;
+            cookieconv = cookiesellraw.ToString();
+            cookieinstasell = Math.Round(double.Parse(cookieconv), 2);
+            label1.Text = "Booster Cookie Sell Price: " + cookieinstasell.ToString();
+             
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        public string json = @"";
+
+        
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+    }
+}
